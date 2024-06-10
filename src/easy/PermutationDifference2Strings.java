d9a3f537259e1e3ee4409fe1419191f1478558fa
evand <evander.willian@gmail.com>
2024-06-10 10:49:37 -0300

    getting better solution than my for study

--- src/easy/PermutationDifference2Strings.java
+++ src/easy/PermutationDifference2Strings.java
@@ -1,19 +1,31 @@
 package src.easy;
 
+import java.util.HashMap;
+
 public class PermutationDifference2Strings {
  //abc bac
     public int findPermutationDifference(String s, String t) {
         int result = 0;
         for (int i = 0; i < s.length(); i++) {
             for (int j = 0; j < t.length(); j++) {
-                char sChar = s.charAt(i);
-                char tChar = t.charAt(j);
-                if(sChar == tChar) {
+                if(s.charAt(i) == t.charAt(j)) {
                     result += Math.abs(i - j);
                 }
             }
         }
         return result;
+    }
+
+    public int findPermutationDifferenceWithHash(String s, String t) {
+        HashMap<Character, Integer> map = new HashMap();
+        for (int i = 0; i < s.length(); i++){
+            map.put(s.charAt(i), i);
+        }
+        int ans = 0;
+        for (int i = 0; i < t.length(); i++){
+            ans += Math.abs(i - map.get(t.charAt(i)));
+        }
+        return ans;
     }
 
 }
