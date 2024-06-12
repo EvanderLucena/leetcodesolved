428a31737c9396d94f72fcd575d1b9ed27f85124
evand <evander.willian@gmail.com>
2024-06-12 17:06:29 -0300

    solved with hashmap and a better solution after submitted

---
+++ src/easy/MostWordsFoundSentence.java
@@ -0,0 +1,30 @@
+package src.easy;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class MostWordsFoundSentence {
+    public int mostWordsFound(String[] sentences) {
+        HashMap<Integer, String> stringHashMap = new HashMap<>();
+        int i = 0;
+        int sum = 0;
+        for (String word : sentences) {
+            stringHashMap.put(i, word);
+            i++;
+        }
+        for (String sentence : stringHashMap.values()) {
+            long count = Arrays.stream(sentence.split(" ")).count();
+            if (count > sum) {
+                sum = (int) count;
+            }
+        }
+        return sum+1;
+    }
+    public int mostWordsFoundBetter(String[] sentences) {
+        int count=0;
+        for(String s:sentences){
+            count=Math.max(count,s.split(" ").length);
+        }
+        return count;
+    }
+}
