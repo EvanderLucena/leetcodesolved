1a04bb93ff0653f53387d190a8727653329ef3e7
evand <evander.willian@gmail.com>
2024-06-13 14:51:07 -0300

    cleaning imports

--- src/medium/PartIntoMinNumber1689.java
+++ src/medium/PartIntoMinNumber1689.java
@@ -1,11 +1,5 @@
 package src.medium;
 
-import java.util.HashMap;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.OptionalInt;
-import java.util.stream.IntStream;
-
 public class PartIntoMinNumber1689 {
     public int minPartitions(String n) {
         int max = 0;
